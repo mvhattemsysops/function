b693c1ef1ec3e6273258a6497c5c2173f8354314
Mark van Hattem <mvhattemsysops@outlook.com>
2023-04-28 14:07:29 +0200

    hash script

--- #Start.ps1
+++ #Start.ps1
@@ -1,5 +1,8 @@
 #Start
-function receive-output
-{
-    process { write-host $_ -ForegroundColor DarkGreen}
-}
+#region Colors
+$name=get-computerinfo -property csname
+$model=get-computerinfo -property csmodel
+$osname=get-computerinfo -property osname
+$paramout=@{'pcnaam'=$name;'pcmodel'=$model;'pcos'=$osname}
+$outobj = new-object -typename psobject -property $paramout
+#endregion
