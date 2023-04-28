e1216000a13db32d1343482237d6ab2e0dc08611
Mark van Hattem <mvhattemsysops@outlook.com>
2023-04-28 14:12:52 +0200

    graag uploadedn

--- #Start.ps1
+++ #Start.ps1
@@ -4,5 +4,5 @@ $name=get-computerinfo -property csname
 $model=get-computerinfo -property csmodel
 $osname=get-computerinfo -property osname
 $paramout=@{'pcnaam'=$name;'pcmodel'=$model;'pcos'=$osname}
-$outobj = new-object -typename psobject -property $paramout
+$outobj =  new-object -typename psobject -property $paramout
 #endregion
